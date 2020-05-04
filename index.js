404d9168e0cb1fe22e2be6ec02264331634d0d71
Lucas Turci <lucas.turci@gmail.com>
2020-05-04 20:34:24 -0300

    Setup socket.io
    - In this commit we setup socket io for server-side and client-side
    - Add socket io server in index.js to listen for connections in port 8081
    - Add socket io client in src/index.js to connect to localhost in port 8081
    - After running 'node index.js' and 'yarn webpack-dev-server', node should log a message saying a new user has connected after you open localhost:8080 in the web browser

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
-const port = 3000;
+const port = 8081
 http.listen(port, () => {
     console.log("Listening on port ", port)
 })
+
+io.on('connection', socket => {
+    console.log('New user connected')
+})
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import io from 'socket.io-client'
+
 console.log("Running webpack");
 
+const socket = io('http://127.0.0.1:8081')
 ReactDOM.render(<h1> Hello World </h1>, document.getElementById('root'))

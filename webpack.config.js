1f22b19cc2aa1f0b84b28dfde574e9dff28b8094
Lucas Turci <lucas.turci@gmail.com>
2020-05-05 14:22:08 -0300

    Enable sourcemaps for development envirotnment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path')
 const { EnvironmentPlugin } = require('webpack')
 
-module.exports = {
+const devOptions = process.env.NODE_ENV === 'development'? {
+    devServer: {
+        historyApiFallback: true
+    },
+    mode: 'development',
+    devtool: 'source-map'
+} : {}
+
+module.exports = Object.assign({
     entry: path.join(__dirname, 'src', 'index'),
     output: {
         path: path.join(__dirname, 'dist'),
@@ -16,12 +24,10 @@ module.exports = {
             use: 'babel-loader',
         }]
     },
-    devServer: {
-        historyApiFallback: true
-    },
+    
     plugins: [
         new EnvironmentPlugin({
             NODE_ENV: 'development'
         })
     ]
-};
+}, devOptions);

fabbd563b2883a1da0856aa0e6961352d00488b6
Lucas Turci <lucas.turci@gmail.com>
2020-05-06 22:26:33 -0300

    Begin implementation of game state machine, being triggered by messages over sockets

--- src/App.js
+++ src/App.js
@@ -1,51 +1,73 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Menu from './Menu'
 import './style.css'
 /* import {useState, useContext, useCallback } from 'react' */
 
 import { getSocketContext } from './SocketContext'
 
+// Game States
+const MENU = 'MENU'
+const WAITING_OPPONENT = 'WAITING_OPPONENT'
+const WAITING_READY = 'WAITING_READY'
+// const PLAYING = 'PLAYING'
+// const FINISHED = 'FINISHED'
+
 const App = () => {
 	const socket = useContext(getSocketContext())
 
+	const [gameState, setGameState] = useState(MENU)
+
 	useEffect(() => {
+		// SOCKET MESSAGES
+
+		/* 
+            Description: Sends a message that should be alerted to user
+            What should we do?
+            - Alert the message to the user
+        */
 		socket.on('ALERT_ERR', (msg) => {
 			alert(`Error: ${msg}`)
 		})
 
+		/* 
+            Description: Tells that user has successfully joined the room
+            What should we do?
+            - Change game state to WAITING_OPPONENT
+            - Change window to game window
+        */
 		socket.on('JOINED', (roomId) => {
 			alert(`Joined room ${roomId}`)
+			setGameState(WAITING_OPPONENT)
+		})
+
+		/* 
+            Description: Tells the user that its opponent has already joined room and asks if you're ready to start
+            What should we do?
+            - Change game state to WAITING_READY
+            - Enable 'READY' button in game, that if pressed emits a "READY!" message to server
+        */
+		socket.on('READY?', () => {
+			// For now, just alert that both players joined the room
+			alert('Are you ready?')
+			setGameState(WAITING_READY)
 		})
 	}, [])
-	/*     const [chat, setChat] = useState([])
-    const socket = useContext(getSocketContext())
 
-    const sendMessage = useCallback(() => {
-        const input = document.getElementById('message')
-        const message = input.value
-    
-        socket.emit('chat message', message)
-    }, [socket])
-
-    socket.on('chat message', msg => {
-        setChat(chat.concat(msg))
-    })
-
-
-    return <div>
-        <input id='message' type='text' />
-        <button onClick={() => sendMessage()}> Enviar </button>
-
-        <div>
-            {chat.map(msg => <p> {msg} </p>)}
-        </div>
-    </div> */
-
-	return (
+	const menu = (
 		<div className="column">
-			<h1> VIKINGS </h1> <Menu />
+			<h1> VIKINGS </h1>
+			<Menu />
 		</div>
 	)
+
+	const game = (
+		<div className="column">
+			<h1> VIKINGS </h1>
+			<button disabled={gameState !== WAITING_READY}> Ready! </button>
+		</div>
+	)
+
+	return gameState === MENU ? menu : game
 }
 
 export default App

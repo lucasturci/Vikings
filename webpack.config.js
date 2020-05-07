b4ceb4227286df84c8b8ee52f38e82151ee53d39
Lucas Turci <lucas.turci@gmail.com>
2020-05-07 19:05:21 -0300

    Add file-loader to webpack so you can import images to use inside react components

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = Object.assign(
 					test: /\.css$/,
 					use: ['style-loader', 'css-loader'],
 				},
+				{
+					test: /\.(png|svg|jpg|jpeg|gif)$/,
+					use: ['file-loader'],
+				},
 			],
 		},
 
